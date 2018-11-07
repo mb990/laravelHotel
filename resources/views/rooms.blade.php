909289cf5224b5414c1e2b4f0d174422b8c963f4
mb990 <stayforff@gmail.com>
2018-11-07 14:40:23 +0100

    rooms view update

--- resources/views/rooms.blade.php
+++ resources/views/rooms.blade.php
@@ -5,6 +5,9 @@
 @section('content')
 
 <div class="container">
+	<div class="col-lg-12 col-md-12 col-sm-12">
+		<h2 class="text-center">Available rooms</h2>
+	</div>
 	<div class="row">
 		<div class="float-left col-lg-4 col-md-4 col-sm-4">
 			<img class="room_item" src="https://s-media-cache-ak0.pinimg.com/originals/1e/97/24/1e9724023761181bcb976a1eec850832.jpg">
