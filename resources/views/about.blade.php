63177c6bcd82fe668c76b4b8e0bd56a578e9afab
mb990 <stayforff@gmail.com>
2018-11-08 18:25:24 +0100

    git test from laptop

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -11,4 +11,6 @@
 	cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
 	proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
 
+	<p>proba za git</p>
+
 @endsection
