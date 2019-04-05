8b4c9666940a4719df4cb5b90c706962f2ff1a0d
mb990 <stayforff@gmail.com>
2019-04-05 20:44:12 +0200

    added search news functionality for website users

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -7,16 +7,22 @@ use Illuminate\Support\Facades\DB;
 
 class SearchController extends Controller
 {
-   public function search_news (Request $request){
-    $search = $request->get('news');
-    $news = DB::table('news')->where('title', 'like', '%' . $search . '%')->paginate(5);
-    return view ('admin.news.news')->with('news', $news);
+    public function search_news (Request $request){
+        $search = $request->get('news');
+        $news = DB::table('news')->where('title', 'like', '%' . $search . '%')->paginate(5);
+        return view ('news.news')->with('news', $news);
+       } 
+
+    public function admin_search_news (Request $request){
+        $search = $request->get('news');
+        $news = DB::table('news')->where('title', 'like', '%' . $search . '%')->paginate(5);
+        return view ('admin.news.news')->with('news', $news);
    } 
 
-   public function search_users (Request $request){
-    $search = $request->get('users');
-    $users = DB::table('users')->where('name', 'like', '%' . $search . '%', 'or', 'email', 'like', '%' . $search . '%')->paginate(5);
-    return view ('admin.users.users')->with('users', $users);
+    public function admin_search_users (Request $request){
+        $search = $request->get('users');
+        $users = DB::table('users')->where('name', 'like', '%' . $search . '%', 'or', 'email', 'like', '%' . $search . '%')->paginate(5);
+        return view ('admin.users.users')->with('users', $users);
    } 
     
 }
--- resources/views/news/news.blade.php
+++ resources/views/news/news.blade.php
@@ -13,5 +13,11 @@
 		@endforeach
 	</ul>
 </div>
+<div id="search">
+		<form action="/admin/news/search" method="GET">
+			<input type="text" name ="news" placeholder="Search News"/>
+			<button type="submit" class="tip-bottom" title="Search"><i class="icon-search icon-white">Search</i></button>
+		</form>
+</div>
 <div class="lead pagination"> {{ $news->links() }} </div>
 @endsection
